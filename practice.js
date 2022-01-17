86cf711694c163362b50560990249a3bd2fa4780
anwar_moazam <anwar.palimarwar@gmail.com>
2022-01-17 10:39:10 +0530

    practice

--- practice.js
+++ practice.js
@@ -1,1 +1,13 @@
 console.log("This is Course - JavaScript The New Hard Parts");
+
+// Iterators in JavaScript
+
+let myArray = [100,200,300,400,500];
+
+function iteratorElement(arr){
+    let index = 0;
+
+}
+
+// Promise.all
+// Promise.race
